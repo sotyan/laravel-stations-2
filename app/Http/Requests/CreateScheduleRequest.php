1c6ac577da7f5f0a63705628fe89c96d4dafb3e6
sotyan <happy.tanbarin2@gmail.com>
2024-06-30 09:50:55 +0900

    Station16Complete

--- app/Http/Requests/CreateScheduleRequest.php
+++ app/Http/Requests/CreateScheduleRequest.php
@@ -31,22 +31,33 @@ class CreateScheduleRequest extends FormRequest
             'start_time_date' => ['required', 'date_format:Y-m-d', 'before_or_equal:end_time_date'],
             //'start_time_time' => ['required', 'date_format:H:i', 'before:end_time_time'],
             'start_time_time' => ['required', 'date_format:H:i', 'before:end_time_time', function ($attribute, $value, $fail) {
+                $pattern = '/^(?:\d{4})\-(?:\d{1,2})\-(?:\d{1,2}) (?P<hour>\d{1,2}):(?P<minute>\d{1,2})$/';
+                if (!preg_match($pattern, $this->input('start_time_date') . ' ' . $this->input('start_time_time'), $matches)) {
+                    // Y/m/d H時i分形式ではない場合はエラー処理
+                    $fail('out');
+                    return;
+                }
                 // 開始時刻と終了時刻の差が5分以上あることを確認
                 $startTime = Carbon::createFromFormat('Y-m-d H:i',$this->input('start_time_date') . ' ' . $this->input('start_time_time'));
                 $endTime = Carbon::createFromFormat('Y-m-d H:i',$this->input('end_time_date') . ' ' . $this->input('end_time_time'));
                 $diffInMinutes = $endTime->diffInMinutes($startTime);
-
+                //dd($startTime,$endTime,$diffInMinutes);
                 if ($diffInMinutes <= 5) {
                     $fail('上映時間の差は5分以上にしてください。');
                 }
             }],
             'end_time_date' => ['required', 'date_format:Y-m-d', 'after_or_equal:start_time_date'],
             'end_time_time' => ['required', 'date_format:H:i', 'after:start_time_time', function ($attribute, $value, $fail) {
+                $pattern = '/^(?:\d{4})\-(?:\d{1,2})\-(?:\d{1,2}) (?P<hour>\d{1,2}):(?P<minute>\d{1,2})$/';
+                if (!preg_match($pattern, $this->input('start_time_date') . ' ' . $this->input('start_time_time'), $matches)) {
+                    // Y/m/d H時i分形式ではない場合はエラー処理
+                    $fail('out');   
+                    return;
+                }
                 // 開始時刻と終了時刻の差が5分以上あることを確認
                 $startTime = Carbon::createFromFormat('Y-m-d H:i',$this->input('start_time_date') . ' ' . $this->input('start_time_time'));
                 $endTime = Carbon::createFromFormat('Y-m-d H:i',$this->input('end_time_date') . ' ' . $this->input('end_time_time'));
                 $diffInMinutes = $endTime->diffInMinutes($startTime);
-
                 if ($diffInMinutes <= 5) {
                     $fail('上映時間の差は5分以上にしてください。');
                 }
